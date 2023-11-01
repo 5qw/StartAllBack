822946c31291d56b439a016564315af3f4785b49
$ <84581959+5qw@users.noreply.github.com>
2023-11-01 04:35:48 -0700

    upload

---
+++ StartAllBack.ps1
@@ -0,0 +1,42 @@
+﻿$Host.UI.RawUI.WindowTitle = "StartAllBack trial reset || https://glock.rip/cat"
+Write-Output  'CAUTION!! Use this scripts at your own risk. I am not responsible for any damage that may occur as a result of running this script.'
+$x = Read-Host 'Do you want to begin? [Y/N]'
+if (-Not($x -eq "Y" -or ($x -eq "y"))) {
+    exit
+}
+
+$keys = Get-Item -Path Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\* | Select-Object -ExpandProperty Name
+$keys = $keys | Where-Object { $_ -cmatch '\{[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{8}.*$' }
+
+foreach ($key in $keys) {
+    $subkeys = Get-Item -Path ('Registry::' + $key + "\*") | Select-Object -ExpandProperty Name
+    $subkeys_count = $subkeys | Measure-Object | Select-Object -ExpandProperty Count
+    if (-Not ($subkeys_count -eq 0)) {
+        continue
+    }
+
+    Write-Output  '' 
+    Write-Output  ('Cleaning...')
+    Write-Output  '' 
+    Remove-Item -Path ('Registry::' + $key)
+    
+    
+}
+
+Write-Output  ('Cleaned Successfully!')
+
+$y = Read-Host 'Explorer restart is required, do you want to continue? [Y/N]'
+if (-Not($y -eq "Y" -or ($y -eq "y"))) {
+    Write-Output  ('Remember restart the explorer later.')
+    exit
+}
+
+stop-process -name explorer –force
+Write-Output  '' 
+Write-Output  'All DONE' 
+Write-Output  '' 
+Write-Host "Press any key to Exit..."
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait('%{F4}')
